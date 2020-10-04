2693b1b67e731ccc7201e8b2c9733925e543e8cf
huiter rodrigues <61643960+huiterjrv@users.noreply.github.com>
2020-10-04 12:14:17 +0200

    subida a git

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,7 @@ import avatar from 'images/marisell.png'
 function Home () {
 
     return <section className='Home'>
-        <h2>Gracias por vicitarme</h2>
+        <h2>Gracias por visitarme</h2>
         <img className='avatar' src={avatar} alt='Holaaa.. soy yo bueno mi avatar' />
     </section>
 }
